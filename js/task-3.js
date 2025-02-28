2654e90c082f3140cc7b74702c2c7f97f2ab8356
demyanyshyn <taras.demyanyshyn@gmail.com>
2025-02-28 12:02:01 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,23 @@
 function getElementWidth(content, padding, border) {
-  return Number.parseFloat(content)+Number.parseFloat(padding)*2 + Number.parseFloat(border)*2;
+  return (
+    Number.parseFloat(content) +
+    Number.parseFloat(padding) * 2 +
+    Number.parseFloat(border) * 2
+  );
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    console.log(response);
+    // Response handling
+  })
+  .then((data) => {
+    console.log(data);
+    // Data handling
+  })
+  .catch((error) => {
+    // Error handling
+  });
